54d3c859d0efb31464e41291bf9566c191512816
GalitSha <galit.sharon@mail.huji.ac.il>
2018-05-18 13:24:16 +0300

    change before open web

---
+++ js_chocolate.js
@@ -0,0 +1,30 @@
+function playNav() {
+  let lastShown = $(".workWindow");
+
+  $(".workshops").click(function() {
+    lastShown.hide();
+    $(".workWindow").show();
+     lastShown = $(".workWindow");
+  });
+
+  $(".pralines").click(function() {
+   lastShown.hide();
+    $(".praWindow").show();
+    lastShown = $(".praWindow");
+  });
+
+  $(".enclosures").click(function() {
+    lastShown.hide();
+    $(".encloWindow").show();
+    lastShown = $(".encloWindow");
+  });
+
+  $(".about").click(function() {
+    lastShown.hide();
+    $(".aboutWindow").show();
+    lastShown = $(".aboutWindow");
+  });
+}
+
+
+$(document).ready(playNav)
